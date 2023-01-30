4dbc2dfa06c8073d5be26f110f754085e3858e6b
Dorkside <james.martin@axa.com>
2023-01-30 11:31:28 +0100

    test: log vue isntance

--- packages/test-vue/src/components/FederatedModule.vue
+++ packages/test-vue/src/components/FederatedModule.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive } from "vue";
+import { ref, reactive, getCurrentInstance } from "vue";
 
 const state = reactive({ count: 0 });
 const countRef = ref(0);
@@ -9,6 +9,8 @@ setInterval(() => {
   countRef.value += 1;
   console.log(state, countRef);
 }, 1000);
+
+console.log("Vue instance", getCurrentInstance());
 </script>
 <template>
   <main>
